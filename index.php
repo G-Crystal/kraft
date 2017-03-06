e212e40ae665adee16fe44c53ce1b2f1b1e54a55
Crystal <jeremy85828@gmail.com>
2017-03-07 06:39:38 +0800

    WIP - updated main structure on index

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<?php 
+    include_once("config.php");
+?>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title><?php echo SITE_TITLE;?></title>
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+    <!-- Custom Theme CSS -->
+    <link href="~/media/KraftHeinz/FoodServiceCA/Files/CSS/styles-Article-GrilledCheese.css" rel="stylesheet">
+
+    <!-- Latest compiled and minified jQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    
+    <!-- Latest compiled and minified Bootstrap -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="~/media/KraftHeinz/FoodServiceCA/Files/js/script.js"></script>
+</head>
+
+<body>
+
+<div class="container">
+    <?php include("header.php");?>
+
+    <?php include("main.php");?>
+</div>
+
+</body>
+</html>
