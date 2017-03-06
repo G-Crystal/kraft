ea3991023246cf5c4248f54286dbbdf8a887df34
Crystal <jeremy85828@gmail.com>
2017-03-07 07:33:00 +0800

    WIP - added language files

---
+++ ~/media/KraftHeinz/FoodServiceCA/language/english.php
@@ -0,0 +1,31 @@
+<?php
+
+/*******************************************************************\
+
+ * Kraft Heinz
+
+ * http://www.kraftheinz.com
+
+ * Written By Jeremy Lee
+
+ * Copyright (c) 2017-2017 KraftHeinz Software. All rights reserved.
+
+\*******************************************************************/
+
+
+/* HEADER MENU */
+
+define("SITE_TITLE", "Kraft Heinz");
+define("LOGO_TITLE", "Reserved for foodservice professionals.");
+
+define("OFFERS", "Offers");
+define("RECIPE_BOX", "Recipe Box");
+define("SIGN_IN", "Sign In");
+define("SIGN_UP", "Sign Up");
+define("MY_ACCOUNT", "My Account");
+define("LOGOUT", "Logout");
+define("ABBREBIATION_ENGLISH", "EN");
+define("ABBREBIATION_FRENCH", "FR");
+define("SEARCH_MSG", "Search");
+define("", "");
+
---
+++ ~/media/KraftHeinz/FoodServiceCA/language/french.php
@@ -0,0 +1,17 @@
+<?php
+
+/*******************************************************************\
+
+ * Kraft Heinz
+
+ * http://www.kraftheinz.com
+
+ * Written By Jeremy Lee
+
+ * Copyright (c) 2017-2017 KraftHeinz Software. All rights reserved.
+
+\*******************************************************************/
+
+
+/* HEADER MENU */
+
