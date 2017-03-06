a93cefd265fe6137dd5546b39863ea82ecc336f8
Crystal <jeremy85828@gmail.com>
2017-03-07 06:40:01 +0800

    WIP - added config.php

---
+++ config.php
@@ -0,0 +1,22 @@
+<?php 
+
+    define("SITE_URL", "/");
+    define("ASSETS_URL", SITE_URL."~/media/KraftHeinz/FoodServiceCA");
+    define("IMAGE_URL", ASSETS_URL."/Files/images");
+    define("SITE_LANGUAGE", "english");
+   
+    $lang = isset($_COOKIE['site_lang']) ? $_COOKIE['site_lang'] : 'english';
+
+    if (isset($lang) && !empty($lang) && file_exists(ASSETS_URL."/language/".$lang.".php"))
+    {
+        define('USER_LANGUAGE', $lang);
+        require_once(ASSETS_URL."/language/".$lang.".php");
+    }
+    else
+    {
+        define('USER_LANGUAGE', SITE_LANGUAGE);
+        require_once(ASSETS_URL."/language/".SITE_LANGUAGE.".php");
+    }
+
+    include_once("functions.php");
+    
