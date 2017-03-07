ef227a7fe3bb8f19216f6a22a6e23fb37e1ada92
Crystal <jeremy85828@gmail.com>
2017-03-07 10:42:59 +0800

    WIP - added intro article

--- main.php
+++ main.php
@@ -2,3 +2,46 @@
     include_once("config.php");
 ?>
 
+<div class="main-div">
+    <div class="content-div row">
+        <div id="section-1 col-xs-12">
+            <div class="row">
+                <div class="col-sm-6 col-xs-12">
+                    <div class="sub-title text-blue">
+                        <?php echo The_Ultimate_Grilled_Cheese;?>
+                    </div>
+                    <div class="text-blue">
+                        <h4><?php echo Melt_their_hearts_with_modern_takes_on_this_beloved_classic;?><h4>
+                    </div>
+                    <div class="margin-10-0">
+                        <img src="<?php echo IMAGE_URL; ?>/flike.png" border="0" />
+                        <span><?php echo Be_the_first_fo_your_friends_to_like_this;?><span>
+                    </div>
+                    <div class="">
+                        <span><b><?php echo Canadians_love_their_grilled_cheese_sandwiches;?></b><span>
+                        <span><?php echo They_also_love_discovering_new_versions_that_incorporate_modern_flavour_combinations_among_the_cheesy_goodness;?></span>
+                    </div>
+                </div>
+
+                <div class="col-sm-6 col-xs-12">
+                    <img class="full-img" src="<?php echo IMAGE_URL; ?>/wr0217_kfs_benefits_carousel_1.jpg" border="0" />
+                    <span><u><?php echo Moroccan_Chicken_Grilled_Cheese;?></u><span>
+                </div>
+            </div>
+        </div>
+
+        <div id="section-2 col-xs-12">
+            <div class="main-gadget-1">
+                <div class="grilled_title"><span><b><?php echo APRIL;?></b><?php echo IS_NATIONAL;?></span></div>
+                <div class="grilled_title"><span class="text-yellow"><?php echo GRILLED_CHEESE;?></span><span><?php echo MONTH;?></span></div>
+                <div><span><?php echo Up_your_menu_offerings_with_these_unique_twists;?></span></div>
+            </div>
+            <div class="">
+                <img class="" src="<?php echo IMAGE_URL; ?>/pan.png" border="0" />
+            </div>
+        </div>
+
+        <div id="section-3 col-xs-12">
+        </div>
+    </div>
+</div>
--- ~/media/KraftHeinz/FoodServiceCA/language/english.php
+++ ~/media/KraftHeinz/FoodServiceCA/language/english.php
@@ -27,5 +27,16 @@ define("LOGOUT", "Logout");
 define("ABBREBIATION_ENGLISH", "EN");
 define("ABBREBIATION_FRENCH", "FR");
 define("SEARCH_MSG", "Search");
+define("The_Ultimate_Grilled_Cheese", "The Ultimate Grilled Cheese");
+define("Melt_their_hearts_with_modern_takes_on_this_beloved_classic", "Melt their hearts with modern takes on this beloved classic.");
+define("Be_the_first_fo_your_friends_to_like_this", "Be the first fo your friends to like this.");
+define("Canadians_love_their_grilled_cheese_sandwiches", "Canadians love their grilled cheese sandwiches—they’re familiar, dependable and craveable.");
+define("They_also_love_discovering_new_versions_that_incorporate_modern_flavour_combinations_among_the_cheesy_goodness", "They also love discovering new versions that incorporate modern flavour combinations among the cheesy goodness. Look to the proliferation of single-focus grilled cheese concepts popping up across the country, such as MELT\"wich\" Food Co. and Gorilla Cheese. Bold, fun flavour combinations are the order of the day.");
+define("Moroccan_Chicken_Grilled_Cheese", "Moroccan Chicken Grilled Cheese");
+define("APRIL", "APRIL");
+define("IS_NATIONAL", " IS NATIONAL");
+define("GRILLED_CHEESE", "GRILLED CHEESE");
+define("MONTH", "MONTH");
+define("Up_your_menu_offerings_with_these_unique_twists", "Up your menu offerings with these unique twists.");
 define("", "");
 
